9a94f79279db4aa5c84219ddeb9f54b9516c1610
Thomas Peterson <hidinginthebbc@gmail.com>
2018-11-17 15:01:02 +0000

    Filtering out unnecessary columns
    This removes the TTL and > 4th nameserver from the outputted CSV. Record class
    stays in place to help distinguishing between valid responses, NXDOMAIN, and
    SERVFAIL.

--- bin/cname_dig.sh
+++ bin/cname_dig.sh
@@ -5,5 +5,8 @@
 export DOMAIN=$1
 export A_OR_CNAME=$(dig +noall +answer www.${DOMAIN} 2>/dev/null | head -1 | awk '{$1=$1}1' ORS="" OFS=",");
 export NS=$(dig +short NS ${DOMAIN} 2>/dev/null | awk -v ORS="," '2'| sed s'/,$//');
-echo "${DOMAIN},${A_OR_CNAME},${NS}";
 
+echo "${DOMAIN},${A_OR_CNAME},${NS}" |\
+  # Only print select fields:
+  # domain, www, class, record type, answer, and first four name servers
+  awk -F, '{OFS=","};{print $1,$2,$4,$5,$6,$7,$8,$9,$10}';
