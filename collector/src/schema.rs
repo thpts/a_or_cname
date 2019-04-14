8be8163f3b556b070214de76de6181a7fd88925c
Thomas Peterson <hidinginthebbc@gmail.com>
2019-04-14 21:31:47 +0100

    Adding schema for the record table

--- collector/src/schema.rs
+++ collector/src/schema.rs
@@ -1,4 +1,5 @@
 table! {
+    /// # Domain
     /// This table represents the domains to be queried and their ranking.
     /// For the sake of further analysis, we persist not only the fully qualified
     /// domain name (FQDN), but also its constituent components, however we have
@@ -29,5 +30,41 @@ table! {
 
         /// Suffix, e.g "co.uk"
         suffix -> Text,
+    }
+}
+
+table! {
+    /// # Record
+    /// This table represents the answers performed for the DNS queries made. Each row represents a
+    /// record returned from any number of DNS queries performed - one query may result in `n`, for
+    /// example a query for `IN A example.com` may return `CNAME example.net` and `A 127.0.0.1`.
+    record (domain) {
+
+        /// row-id of the domain from which the DNS query was derived from.
+        domain -> Integer,
+
+        /// If the record is a child, (e.g. the A of a CNAME), this integer refers to the row-id
+        /// of the parent.
+        parent -> Integer,
+
+        /// RCODE value from the Answer (see
+        /// [RFC 1035 &sect; 4.1.1](https://tools.ietf.org/html/rfc1035#section-4.1.1)) - this value
+        /// is persisted to distinguish failures such as receiving `NXDOMAIN`.
+        response_code -> Integer,
+
+        /// Record Type, e.g "CNAME", "A" etc.
+        record_type -> Text,
+
+        /// The record's Time To Live value, which may have applicability in future understanding
+        /// record freshness across the dataset.
+        ttl -> Integer,
+
+        /// Address provided in the record, this may be a FQDN or IP address.
+        address -> Text,
+
+        /// The Autonomous System Number assigned for any IP addresses found in the address. This is
+        /// not provided in the DNS response but by matching an address value that equals an IP
+        /// address against a database of CIDR ranges to match against it.
+        asn -> Integer,
     }
 }
