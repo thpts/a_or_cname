02c3aa5b7dd10a082abcd9e4b98c5c820ed9561c
Thomas Peterson <hidinginthebbc@gmail.com>
2019-05-19 12:41:00 +0000

    Minor doc improvement

--- collector/src/model/mod.rs
+++ collector/src/model/mod.rs
@@ -5,8 +5,15 @@ use diesel::prelude::*;
 
 /// Open the SQLite database for use
 ///
-/// Arguments:
+/// # Arguments:
 /// * `database_url` - Path to the SQLite file
+///
+/// # Example
+/// ```
+/// use model::connect;
+///
+/// let conn = connect(":memory:");
+/// ```
 pub fn connect(database_url: String) -> SqliteConnection {
     SqliteConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
