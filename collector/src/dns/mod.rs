b11a1e127aa86f613fab4def8d1d815191aa4a84
Thomas Peterson <hidinginthebbc@gmail.com>
2019-06-10 14:22:31 +0100

    Fixes to the doctests

--- collector/src/dns/mod.rs
+++ collector/src/dns/mod.rs
@@ -8,12 +8,17 @@ use publicsuffix::Domain;
 /// sub-domain from a root domain, which we need when performing tasks against
 /// the root of a domain.
 ///
-/// ```text (FIXME: Should be doctest that can run)
+/// ```
 /// use publicsuffix::{Domain, List};
 /// use damp::dns::get_sub_domain;
 ///
-/// let domain = List.from_domain("foo.example.co.uk")
-/// assert_eq!(get_sub_domain(domain), Some("foo".to_string()));
+/// // Note: This unusual use of loading the publicsuffix list prevents this
+/// // documentation test from going on the wire and is not recommended for
+/// // real world usage.
+///
+/// let list = List::from_str("BEGIN ICANN DOMAINS\ncom\n").unwrap();
+/// let domain = list.parse_domain("foo.example.com").unwrap();
+/// assert_eq!(get_sub_domain(&domain), Some("foo".to_string()));
 /// ```
 pub fn get_sub_domain(domain: &Domain) -> Option<String> {
     let root_domain = match domain.root() {
@@ -31,6 +36,15 @@ pub fn get_sub_domain(domain: &Domain) -> Option<String> {
 
 /// Get the root domain, sans the suffix.
 /// Thus from `example.com` return `example`.
+///
+/// ```
+/// use publicsuffix::{Domain, List};
+/// use damp::dns::get_root_domain;
+///
+/// let list = List::from_str("BEGIN ICANN DOMAINS\ncom\n").unwrap();
+/// let domain = list.parse_domain("example.com").unwrap();
+/// assert_eq!(get_root_domain(&domain), Some("example".to_string()));
+/// ```
 pub fn get_root_domain(domain: &Domain) -> Option<String> {
     let root_domain = match domain.root() {
         Some(domain) => domain,
