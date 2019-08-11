f22f9643e7b2e7063a0c0faf8518c34a191ca673
Thomas Peterson <hidinginthebbc@gmail.com>
2019-08-11 19:18:45 +0100

    Using failure library for error handling

--- collector/src/bin/database_setup.rs
+++ collector/src/bin/database_setup.rs
@@ -1,5 +1,6 @@
 extern crate chrono;
 extern crate clap;
+extern crate failure;
 #[macro_use]
 extern crate diesel_migrations;
 
@@ -9,7 +10,7 @@ use clap::{App, Arg};
 use damp::model::connect;
 use damp::{end_processing_marker, start_processing_marker};
 use diesel_migrations::embed_migrations;
-use std::error::Error;
+use diesel_migrations::RunMigrationsError;
 
 static QUERY_VERSION: &'static str = env!("CARGO_PKG_VERSION");
 static QUERY_AUTHORS: &'static str = env!("CARGO_PKG_AUTHORS");
@@ -19,7 +20,7 @@ Generates a correct version of the database with the necessary schema.
 
 embed_migrations!("./migrations");
 
-fn main() -> Result<(), Box<Error>> {
+fn main() -> Result<(), RunMigrationsError> {
     let matches = App::new("database_setup")
         .version(QUERY_VERSION)
         .author(QUERY_AUTHORS)
@@ -34,14 +35,12 @@ fn main() -> Result<(), Box<Error>> {
         .get_matches();
 
     let sqlite_db = matches.value_of("sqlite-db").unwrap();
-
     let start =
         start_processing_marker(format!("Loading of schema into {}", sqlite_db.to_string()));
-
     let conn = connect(sqlite_db.to_string());
 
     return match embedded_migrations::run_with_output(&conn, &mut std::io::stdout()) {
-        Err(e) => Err(Box::new(e)),
+        Err(e) => Err(e),
         Ok(_) => {
             end_processing_marker("Migrations completed", start);
             Ok(())
