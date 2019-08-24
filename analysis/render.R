6a66aed29e34b3c94f7f77216fa91cf1eccdd486
Thomas Peterson <hidinginthebbc@gmail.com>
2019-08-24 23:04:31 +0100

    Adding new notebook for putting analysis together
    This is deliberately using the TF template from the `rticles` module as there's
    been too much grief trying to get it to work inside the Docker container.
    Instead, the intention is to use this notebook and port it later into the
    correct format after the data has been analysed.
    
    You will also note this is working off a "snapshot" that is not checked in, the
    full complete dataset will replace it in due course but any reference to values
    will be templated to avoid referring to incorrect values.

--- analysis/render.R
+++ analysis/render.R
@@ -2,4 +2,4 @@ library(rmarkdown)
 
 dataDir <- '/data/'
 
-render('ietf-dnsop-2018-11-21/index.Rmd', c("html_document", "pdf_document"))
+render('journal-cyber-policy-2019/index.Rmd', c("pdf_document", "html_document"))
